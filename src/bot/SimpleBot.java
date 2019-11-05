f014a0f96908f7f8a03802bd39e0edd89d663a0f
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-11-05 17:47:45 +0100

    Stage 2 completed

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -1,8 +1,26 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! My name is Weebo.");
-        System.out.println("I was created in 2019");
+        System.out.println("I was created in 2019.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+
+        System.out.printf("What a great name you have, %s!\n", name);
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.printf("Your age is %d; that's a good time to start programming!", age);
     }
 }
