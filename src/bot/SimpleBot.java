f0bdd024c1b485babb45f040c38a5b030a0b1940
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-11-06 20:18:14 +0100

    Stage 4 completed

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -3,32 +3,60 @@ package bot;
 import java.util.Scanner;
 
 public class SimpleBot {
+    final static Scanner scanner = new Scanner(System.in); // Do not change this line
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        greet("Weebo", "2019"); // change it as you need
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
 
-        System.out.println("Hello! My name is Weebo.");
-        System.out.println("I was created in 2019.");
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
+    }
 
+    static void remindName() {
         String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
 
-        System.out.printf("What a great name you have, %s!\n", name);
+    static void guessAge() {
         System.out.println("Let me guess your age.");
-        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
 
-        int remainder3 = scanner.nextInt();
-        int remainder5 = scanner.nextInt();
-        int remainder7 = scanner.nextInt();
-        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
-
-        System.out.printf("Your age is %d; that's a good time to start programming!\n", age);
+    static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
-
-        int count = scanner.nextInt();
-        for (int i = 0; i <= count; i++){
-            System.out.println(i + "!");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
         }
+    }
 
-        System.out.println("Completed, have a nice day!");
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Which of these statements can skip processing the remainder of code in its body for a particular iteration?");
+        System.out.println("1. break");
+        System.out.println("2. return");
+        System.out.println("3. continue");
+        System.out.println("4. exit");
+
+        while (scanner.nextInt() != 3){
+            System.out.println("Please, try again.");
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!");
     }
 }
